d57ce130432f54d59876365aa8434988b840cc6c
Edson <edbdelima@gmail.com>
2023-03-28 22:32:32 -0300

    Adicionar arquivos de projeto.

---
+++ az204-cosmos/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.Cosmos;
+
+public class Program
+{
+    private static readonly string EndpointUri = "<documentEndpoint>";
+
+    private static readonly string PrimaryKey = "<your primary key>";
+
+    // The Cosmos client instance
+    private CosmosClient cosmosClient;
+
+    // The database we will create
+    private Database database;
+
+    // The container we will create
+    private Container container;
+
+    // The names of the database and container we will create
+    private string databaseId = "az204Database";
+    private string containerId = "az204Container";
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Beginning operations...\n");
+            Program program = new Program();
+            await program.CosmosAsync();
+        }
+        catch (CosmosException de)
+        {
+            Exception baseException = de.GetBaseException();
+            Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("End of program, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public async Task CosmosAsync()
+    {
+        // A new instance of Cosmos Client is created
+        cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+        // Runs the CreateDatabaseAsync method
+        await CreateDatabaseAsync();
+
+        // Run the CreateContainerAsync method
+        await CreateContainerAsync();
+    }
+
+    private async Task CreateDatabaseAsync()
+    {
+        // Create a new database if it doesn't exist using the cosmosClient
+        database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+        Console.WriteLine("Created Database {0}", database.Id);
+    }
+
+    private async Task CreateContainerAsync()
+    {
+        // Create a new container
+        container = await database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+        Console.WriteLine("Created Container: {0}\n", container.Id);
+    }
+}
